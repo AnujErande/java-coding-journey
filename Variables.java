b18b75f83d9ab02557bec9f74292cbb4264e5379
Anuj Erande <anujerande2005@gmail.com>
2026-05-06 11:33:04 +0530

    Day 2: Data Types and Variables

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+    
+    public static void main(String[] args){
+        // Variable declaration and initialization
+        // DataType variableName = value;
+        // Example:
+
+        int iNo = 20;   // it creates a variable of type integer and assigns it the value 20
+        System.out.println("Integer value: " + iNo);
+
+        float fNo = 3.14f;
+        System.out.println("Float value: " + fNo);
+
+    }
+}
---
+++ dataType.java
@@ -0,0 +1,45 @@
+public class dataType {
+    public static void main(String[] argd){
+
+        // Primitive Data Types in Java
+
+        // 1. byte, 2. short, 3. int, 4. long, 5. float, 6. double, 7. char, 8. boolean
+
+        //  byte range is -128 to 127
+        byte bNo = 10;
+        System.out.println("Value of bNo is : " + bNo);
+
+        // short range is -32768 to 32767
+        short sNo = 100;
+        System.out.println("Value of sNo is : " + sNo);
+
+        // int range is -2147483648 to 2147483647
+        int iNo = 10;
+        System.out.println("Value of iNo is : " + iNo);
+
+        // long range is -9223372036854775808 to 9223372036854775807
+        long lNo = 10083623570523L;
+        System.out.println("Value of lNo is : " + lNo);
+
+        // float range is used for single precision floating point numbers.
+        float fNo = 3.14f;
+        System.out.println("Value of fNo is : " + fNo);
+
+       // double range is used for double precision floating point numbers.
+        double dNo = 3.14567489;
+        System.out.println("Value of dNo is : " + dNo);
+
+        // char range is 0 to 65535 and it is used to store a single character.
+        char ch = 'A';
+        System.out.println("Value of ch is : " + ch); 
+
+        // boolean range is either true or false.
+        boolean bValue = true;
+        System.out.println("Value of bValue is : " + bValue);
+
+
+
+
+
+    }
+}
